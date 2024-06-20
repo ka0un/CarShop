5e99bf4910ca76f889503b34daf2313fd33bfad2
kasun <88395585+ka0un@users.noreply.github.com>
2024-06-20 14:20:54 +0530

    handled null pointers in Controllers

--- src/main/java/com/hapangama/carshop/vehicle/controllers/VehicleController.java
+++ src/main/java/com/hapangama/carshop/vehicle/controllers/VehicleController.java
@@ -2,6 +2,7 @@ package com.hapangama.carshop.vehicle.controllers;
 
 import com.hapangama.carshop.vehicle.model.Vehicle;
 import com.hapangama.carshop.vehicle.service.VehicleService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,27 +19,70 @@ public class VehicleController {
 
     @GetMapping
     public List<Vehicle> getAllVehicles() {
+
+        //handle exceptions
+        if (vehicleService.getVehicles().isEmpty()) {
+            ResponseEntity.notFound().build();
+        }
+
         return vehicleService.getVehicles();
     }
 
     @GetMapping("/{id}")
     public Vehicle getVehicleById(@PathVariable Integer id) {
+
+        if (id == null) {
+            ResponseEntity.badRequest().build();
+        }
+
+        //if request id is not found
+        if (vehicleService.getVehicle(id) == null) {
+            ResponseEntity.notFound().build();
+        }
+
+
         return vehicleService.getVehicle(id);
     }
 
     @PostMapping
     public void createVehicle(@RequestBody Vehicle vehicle) {
+
+        if (vehicle == null) {
+            ResponseEntity.badRequest().build();
+        }
+
+
         vehicleService.addVehicle(vehicle);
     }
 
     @PutMapping("/{id}")
     public void updateVehicle(@PathVariable Integer id, @RequestBody Vehicle vehicle) {
+
+        if (id == null || vehicle == null) {
+            ResponseEntity.badRequest().build();
+        }
+
+        //if update id is not found
+        if (vehicleService.getVehicle(id) == null) {
+            ResponseEntity.notFound().build();
+        }
+
         vehicle.setId(id);
         vehicleService.updateVehicle(vehicle);
     }
 
     @DeleteMapping("/{id}")
     public void deleteVehicle(@PathVariable Integer id) {
+
+        if (id == null) {
+            ResponseEntity.badRequest().build();
+        }
+
+        //if delete id is not found
+        if (vehicleService.getVehicle(id) == null) {
+            ResponseEntity.notFound().build();
+        }
+
         vehicleService.deleteVehicle(id);
     }
 
