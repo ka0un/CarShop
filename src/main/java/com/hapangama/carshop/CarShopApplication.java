618a55a18befbe750af7f85e0f2f6de436c9b473
kasun <88395585+ka0un@users.noreply.github.com>
2024-06-20 11:52:14 +0530

    init

---
+++ src/main/java/com/hapangama/carshop/CarShopApplication.java
@@ -0,0 +1,13 @@
+package com.hapangama.carshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hapangama/carshop/CarShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hapangama.carshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
