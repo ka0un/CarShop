4fc913359f122325b3b4e2eefb4950a605903712
kasun <88395585+ka0un@users.noreply.github.com>
2024-06-21 17:00:13 +0530

    created database creation schema for User

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,9 @@ CREATE TABLE IF NOT EXISTS Vehicle (
                          vehicle_type VARCHAR(255),
                          vehicle_year VARCHAR(4)
 );
+
+CREATE TABLE User (
+                      username VARCHAR(255) NOT NULL PRIMARY KEY,
+                      password VARCHAR(255) NOT NULL,
+                      role ENUM('ADMIN', 'BUYER', 'SELLER') NOT NULL
+);
